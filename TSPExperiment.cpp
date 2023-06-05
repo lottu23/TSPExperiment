e36968a41bf1f4d5e42d875531fa5bc87e16e0a8
l0ttu <lottu2357@outlook.com>
2023-06-05 20:24:35 +0800

    temp-1

--- TSPExperiment.cpp
+++ TSPExperiment.cpp
@@ -7,18 +7,36 @@ using ArcType = float;
 
 const int MaxNodeNum = 100;
 
+/// <summary>
+/// 邻接矩阵结构
+/// </summary>
 typedef struct {
   ArcType arcs[MaxNodeNum][MaxNodeNum];
   int node_num, arc_num;
 } AMGraph;
 
 
+
+void CreateUDN(AMGraph& graph);
+void GetMST(const AMGraph& graph);
+
+int main() {
+  AMGraph graph;
+  CreateUDN(graph);
+  for (int i = 0; i < graph.node_num; i++) {
+    for (int j = 0; j < graph.node_num; j++) {
+      cout << graph.arcs[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
 /// <summary>
 /// 输入数据并生成相应的邻接矩阵
 /// </summary>
 /// <param name="graph">生成的邻接矩阵表</param>
 void CreateUDN(AMGraph& graph) {
-  //输入数据
+  // 输入数据
   ifstream fin;
   fin.open("att8.tsp", ios::in);
   if (!fin.is_open()) {
@@ -32,14 +50,14 @@ void CreateUDN(AMGraph& graph) {
     fin >> a >> pos[i].first >> pos[i].second;
   }
 
-  //初始化结点距离为无限大值
+  // 初始化结点距离为无限大值
   for (int i = 0; i < graph.node_num; i++) {
     for (int j = 0; j < graph.node_num; j++) {
       graph.arcs[i][j] = (ArcType)std::numeric_limits<float>().infinity();
     }
   }
 
-  //将权值设置为结点间的距离
+  // 将权值设置为结点间的距离
   for (int i = 0; i < graph.node_num; i++) {
     for (int j = 0; j < graph.node_num; j++) {
       if (i < j)
@@ -50,13 +68,10 @@ void CreateUDN(AMGraph& graph) {
   }
 }
 
-int main() {
-  AMGraph graph;
-  CreateUDN(graph);
-  for (int i = 0; i < graph.node_num; i++) {
-    for (int j = 0; j < graph.node_num; j++) {
-      cout << graph.arcs[i][j] << " ";
-    }
-    cout << endl;
-  }
+/// <summary>
+/// 使用Prim算法获取最小生成树
+/// </summary>
+/// <param name="graph"></param>
+void GetMST(const AMGraph& graph) {
+
 }
