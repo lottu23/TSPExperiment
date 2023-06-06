febe80e4144e38bb85c50650938ec280e38a8bb7
l0ttu <lottu2357@outlook.com>
2023-06-06 11:53:35 +0800

    temp-2

--- TSPExperiment.cpp
+++ TSPExperiment.cpp
@@ -1,6 +1,8 @@
 ﻿#include <fstream>
 #include <iostream>
 
+#include"randomdata.h"
+
 using namespace std;
 
 using ArcType = float;
@@ -15,21 +17,10 @@ typedef struct {
   int node_num, arc_num;
 } AMGraph;
 
-
-
 void CreateUDN(AMGraph& graph);
 void GetMST(const AMGraph& graph);
 
-int main() {
-  AMGraph graph;
-  CreateUDN(graph);
-  for (int i = 0; i < graph.node_num; i++) {
-    for (int j = 0; j < graph.node_num; j++) {
-      cout << graph.arcs[i][j] << " ";
-    }
-    cout << endl;
-  }
-}
+int main() { GenerateRandomData(50); }
 
 /// <summary>
 /// 输入数据并生成相应的邻接矩阵
@@ -42,30 +33,16 @@ void CreateUDN(AMGraph& graph) {
   if (!fin.is_open()) {
     cout << "open file failed" << endl;
   }
+
   fin >> graph.node_num;
   graph.arc_num = graph.node_num * (graph.node_num - 1) / 2;
-  pair<int, int> pos[MaxNodeNum];
-  int a;
-  for (int i = 0; i < graph.node_num; i++) {
-    fin >> a >> pos[i].first >> pos[i].second;
+
+  int a, b, w;
+  for (int i = 0; i < graph.arc_num; i++) {
+    fin >> a >> b >> w;
+    graph.arcs[a][b] = graph.arcs[b][a] = w;
   }
 
-  // 初始化结点距离为无限大值
-  for (int i = 0; i < graph.node_num; i++) {
-    for (int j = 0; j < graph.node_num; j++) {
-      graph.arcs[i][j] = (ArcType)std::numeric_limits<float>().infinity();
-    }
-  }
-
-  // 将权值设置为结点间的距离
-  for (int i = 0; i < graph.node_num; i++) {
-    for (int j = 0; j < graph.node_num; j++) {
-      if (i < j)
-        graph.arcs[i][j] = graph.arcs[j][i] =
-            sqrt(pow(pos[j].first - pos[i].first, 2) +
-                 pow(pos[j].second - pos[i].second, 2));
-    }
-  }
 }
 
 /// <summary>
---
+++ randomdata.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+const int MaxVexNum = 100;
+
+unsigned int edges[MaxVexNum][MaxVexNum];
+
+int num;
+
+void recur(int node_a, int node_b, int weight);
+
+/// <summary>
+/// 生成随机城市数据
+/// </summary>
+void GenerateRandomData(int n) {
+  srand((unsigned int)time(nullptr));
+  num = 10;
+  // 初始化
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      edges[i][j] = INT_MAX;
+    }
+  }
+  // 设置权重
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      if (i < j) {
+        edges[i][j] = edges[j][i] = rand() % 100 + 1;
+      }
+    }
+  }
+
+  // 检查是否满足三角不等式
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      if (i < j) {
+        for (int k = 0; k < num; k++) {
+          if (k != i && k != j && edges[i][j] > edges[i][k] + edges[k][j]) {
+            edges[i][j] = edges[j][i] = edges[i][k] + edges[k][j] - 1;
+          }
+        }
+      }
+    }
+  }
+
+  // 打印查看
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      cout << edges[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  // 输出到文件
+  ofstream fout;
+  fout.open("data.tsp");
+  if (!fout.is_open()) {
+    cout << "open file failed" << endl; 
+  }
+  fout << num << endl;
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      if (i < j) {
+        fout << i + 1 << " " << j + 1 << " " << edges[i][j] << endl;
+      }
+    }
+  }
+}
