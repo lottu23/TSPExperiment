6c9301a19ad69ddb46dd4e60fa17a2c79c5921df
l0ttu <lottu2357@outlook.com>
2023-06-06 23:00:10 +0800

    temp-4

--- TSPExperiment.cpp
+++ TSPExperiment.cpp
@@ -23,9 +23,10 @@ struct Edge {
   int weight;
 };
 
-void CreateUDN(AMGraph& graph);
-void QuickSort(Edge* edges, int left, int right);
-void GetMST(const AMGraph& graph);
+void CreateUDN(AMGraph &graph);
+void QuickSort(Edge *edges, int left, int right);
+int Find(int *ends, int f);
+void GetMST(const AMGraph &graph);
 
 int main() {
   AMGraph graph;
@@ -49,7 +50,7 @@ int main() {
     }
   }
 
-  QuickSort(edges, 0, graph.arc_num);
+  QuickSort(edges, 0, graph.arc_num - 1);
 
   for (int i = 0; i < k; i++) {
     printf("%d %d %d\n", edges[i].start, edges[i].end, edges[i].weight);
@@ -60,7 +61,7 @@ int main() {
 /// 输入数据并生成相应的邻接矩阵
 /// </summary>
 /// <param name="graph">生成的邻接矩阵表</param>
-void CreateUDN(AMGraph& graph) {
+void CreateUDN(AMGraph &graph) {
   // 输入数据
   ifstream fin;
   fin.open("data.tsp", ios::in);
@@ -90,30 +91,53 @@ void CreateUDN(AMGraph& graph) {
 /// <param name="edges">边的集合</param>
 /// <param name="left">左侧边界</param>
 /// <param name="right">右侧边界</param>
-void QuickSort(Edge* edges, int left, int right) {
-  int l = left, r = right;
-  Edge temp = {};
-  if (l <= r) {
-    temp = edges[left];
-    while (l != r) {
-      while (r > l && edges[r].weight > temp.weight) {
-        r--;
-      }
-      swap(edges[l], edges[r]);
-      while (r > l && edges[l].weight < temp.weight) {
-        l++;
-      }
-      swap(edges[l], edges[r]);
-    }
-    edges[r] = temp;
+void QuickSort(Edge *edges, int left, int right) {
+  if (left > right)
+    return;
+  int i = left, j = right;
+  Edge temp = edges[left];
 
-    QuickSort(edges, left, l - 1);
-    QuickSort(edges, r + 1, right);
+  while (i != j) {
+    while (i < j && edges[j].weight >= temp.weight) {
+      j--;
+    }
+    swap(edges[i], edges[j]);
+    while (i < j && edges[i].weight < temp.weight) {
+      i++;
+    }
+    swap(edges[i], edges[j]);
   }
+
+  QuickSort(edges, i + 1, right);
+  QuickSort(edges, left, i - 1);
+}
+
+/// <summary>
+/// 查找结点对应的终点
+/// </summary>
+/// <param name="ends">结点对应的终点数组</param>
+/// <param name="f">待查找终点的结点</param>
+/// <returns>终点</returns>
+int Find(int *ends, int f) {
+  while (ends[f] > 0) {
+    f = ends[f];
+  }
+  return f;
 }
 
 /// <summary>
 /// 使用Kruskal算法获取最小生成树
 /// </summary>
 /// <param name="graph"></param>
-void GetMST(const AMGraph& graph) {}
+void GetMST(const AMGraph &graph) {
+  Edge edges[MaxNodeNum];
+  int k = 0;
+
+  for (int i = 0; i < graph.node_num; i++) {
+    for (int j = 0; j < graph.node_num; j++) {
+      if (i < j) {
+        edges[k++] = Edge{i, j, graph.arcs[i][j]};
+      }
+    }
+  }
+}
